b2c190ae10a588ad11ebac638c928e678b308114
asotorod <asotorod@gmail.com>
2025-12-29 17:46:19 -0500

    Add hero background image to About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,6 +9,7 @@ function About() {
       <PageHeader 
         title="About Us"
         subtitle="Learn more about Handy AF Home Services"
+        backgroundImage="https://images.unsplash.com/photo-1532009877282-3340270e0529?q=80&w=2340&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
       />
 
       <section className="section about-intro">
